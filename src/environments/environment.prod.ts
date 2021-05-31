63f8a56c9c42a0a4cbc33dc3d09fba4774c243cd
janakas <janakas@codegen.net>
2021-05-31 12:04:17 +0530

    prod configurations added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  appID: '',
+  appID: '9ee668a9ec66350adf0c20b47498656e',
   config: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyCgvxBiFZXLx-H2KSRCuICJQs2jqtwUAik',
+    authDomain: 'minimus-e61a6.firebaseapp.com',
+    databaseURL: 'https://minimus-e61a6-default-rtdb.firebaseio.com/',
+    projectId: 'minimus-e61a6',
+    storageBucket: 'minimus-e61a6.appspot.com',
+    messagingSenderId: '1016808871355'
   }
 };
